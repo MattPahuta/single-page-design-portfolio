f718eee869c6ed39cc7e2a97573eb17e1cd45e63
Matt Pahuta <mhpahuta@gmail.com>
2022-11-10 15:51:27 -0800

    Add/update splide slider, work section styles

--- js/app.js
+++ js/app.js
@@ -1,28 +1,22 @@
-
-console.log('script connected')
-
-// new Splide( '.splide' ).mount();
-
-// document.addEventListener( 'DOMContentLoaded', function () {
-//   new Splide( '#image-carousel', {
-// 		cover      : true,
-//     type       : 'loop',
-//     padding    : '20%',
-//     arrows     : false,
-// 		heightRatio: 0.5,
-//   } ).mount();
-// } );
-
-// new Splide( '.splide', {
-//   type   : 'loop',
-//   padding: '20%',
-// } );
+// Splide slider
+// https://splidejs.com/
 
 document.addEventListener( 'DOMContentLoaded', function () {
   new Splide( '#image-carousel', {
     type   : 'loop',
-    padding: '20%',
-    gap: '1rem',
-    arrows: false,
+    padding: '31%',
+    gap: '1.875rem',
+    arrows: true,
+    pagination: false,
+    autoWidth: true,
+    start: 2,
+    breakpoints: {
+      1150: {
+        padding: '15%',
+      },
+      640: {
+        gap: '1rem',
+      },
+    },
   } ).mount();
 } );
